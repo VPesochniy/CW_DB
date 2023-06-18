3d39e75e924dedc22e70ab7bb6645563d4259556
VPesochniy <vpesochniy@yandex.ru>
2023-06-19 02:11:25 +0300

    Added MENU with READ command

--- cw_db/config.py
+++ cw_db/config.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
 DATE_FORMAT = "%d.%m.%Y"
 TIME_FORMAT = "%H:%M"
 DATETIME_FORMAT = "%H:%M - %d.%m.%Y"
+
+class Menu(Enum):
+    CREATE = 1
+    READ = 2
+    UPDATE = 3
+    DELETE = 4
+    EXIT = 0
